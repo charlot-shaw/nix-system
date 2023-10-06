b99445692465d9326e785aec253aca4461a28bbc
Charlot Shaw <hello@sparrows.dev>
2023-10-06 00:02:56 -0500

    Add Kitty, foreign-env for Fish

--- home-manager/terminal.nix
+++ home-manager/terminal.nix
@@ -1,8 +1,33 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   programs = {
+    kitty = {
+      enable = true;
+      settings = {
+        foreground = "#${config.colorScheme.colors.base05}";
+        background = "#${config.colorScheme.colors.base00}";
+      };
+    };
+
     fish = {
+    # TODO integrate with nix-colors.
+    # Ref https://nix-community.github.io/home-manager/options.html#opt-programs.fish.enable
       enable = true;
       interactiveShellInit = "zoxide init fish | source";
+      plugins = [
+        {
+          name = "foreign env";
+          src = pkgs.fetchFromGitHub {
+            owner = "oh-my-fish";
+            repo = "plugin-foreign-env";
+            rev = "7f0cf099ae1e1e4ab38f46350ed6757d54471de7";
+            sha256 = "4+k5rSoxkTtYFh/lEjhRkVYa2S4KEzJ/IJbyJl+rJjQ=";
+          };
+        }
+      ];
     };
 
     ssh = {
