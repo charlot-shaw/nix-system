e45e909e8ebe49e8f21aa08691d22f3c1d377dd6
Charlot Shaw <hello@sparrows.dev>
2024-04-23 15:41:29 -0500

    Chore: Bump version

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-unstable";
     # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
     # Home manager
@@ -39,7 +39,7 @@
   outputs = {
     self,
     nixpkgs,
-    nixpkgs-unstable,
+    nixpkgs-stable,
     home-manager,
     nix-colors,
     agenix,
