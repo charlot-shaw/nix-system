9d265d9fa4d9125429c83951ffae4354f4c534af
Charlot Shaw <hello@sparrows.dev>
2024-05-07 19:45:07 -0500

    First attempt at NFS

---
+++ nixos/hosts/taproot/nfs.nix
@@ -0,0 +1,12 @@
+{pkgs,config,...}:{
+    filesystems."/export/core" = {
+        options = [ "bind" ]; 
+    };
+
+    services.nfs = {
+        server.enable = true;
+        server.exports = ''
+        /export perch(insecure,rw,sync,no_subtree_check,crossmnt,fsid=0)
+        '';
+    };
+}
