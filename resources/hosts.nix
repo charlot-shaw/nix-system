59d9cb6008c93e76109965def06250ac7ae263d0
Charlot Shaw <hello@sparrows.dev>
2024-05-17 21:17:25 -0500

    Add: Initial knownHosts file

--- resources/hosts.nix
+++ resources/hosts.nix
@@ -1,10 +1,10 @@
-{...}:{
-    let 
-        taproot_ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGu9cjlFdK4M2udGnFIQgRbFVhHAQxUJtKPM6tjHEAt6";
-        perch_ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEWrqm6Axh22waSmP3Z6VuDP+0bf5iqKkSPJa+qM8/U7";
-    in
+let
+  taproot_ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGu9cjlFdK4M2udGnFIQgRbFVhHAQxUJtKPM6tjHEAt6";
+  perch_ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEWrqm6Axh22waSmP3Z6VuDP+0bf5iqKkSPJa+qM8/U7";
+in
+  {...}: {
     programs.ssh.knownHosts = {
-        "taproot".publicKey = taproot_ssh_key;
-        "perch".publicKey = perch_ssh_key;
-    }
-}
+      "taproot".publicKey = taproot_ssh_key;
+      "perch".publicKey = perch_ssh_key;
+    };
+  }
