e087e6456a3c3977c3bf2ff6fe33f1d6299b082b
Charlot Shaw <hello@sparrows.dev>
2024-05-31 19:21:00 -0500

    Feat: Add publish bool to Avahi module.

--- nixos/common/avahi.nix
+++ nixos/common/avahi.nix
@@ -1,7 +1,15 @@
 {pkgs, lib, config, ...}:{
 
     options = {
-        sparrows.avahi.enable = lib.mkEnableOption "Turns on Avahi for local service discovery.";
+        sparrows.avahi = {
+            enable = lib.mkEnableOption "Turns on Avahi for local service discovery.";
+            
+            publish = lib.mkOption {
+                type = lib.types.bool;
+                default = false;
+                description = "Whether to publish local services";
+            };
+            };
     };
 
     config = {
@@ -13,7 +21,7 @@
                 nssmdns4 = true;
 
             
-                publish = {
+                publish = lib.mkIf config.sparrows.avahi.publish {
                     enable = true;
                     userServices = true;
                 };
