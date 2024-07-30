e97df2c87cee453705a3fdb3dfb424190e36cf57
Charlot Shaw <hello@sparrows.dev>
2024-07-29 22:08:05 -0500

    Feat: Add Bough information to hosts.nix

--- resources/hosts.nix
+++ resources/hosts.nix
@@ -1,10 +1,12 @@
 let
   taproot_ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGu9cjlFdK4M2udGnFIQgRbFVhHAQxUJtKPM6tjHEAt6";
   perch_ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEWrqm6Axh22waSmP3Z6VuDP+0bf5iqKkSPJa+qM8/U7";
+  bough_ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMQk6nS9HvbuklDVX5ysR6Bt27L7WBHSkWCE1GJ3goTG";
 in
   {...}: {
     programs.ssh.knownHosts = {
       "taproot".publicKey = taproot_ssh_key;
       "perch".publicKey = perch_ssh_key;
+      "bough".publicKey = bough_ssh_key;
     };
   }
